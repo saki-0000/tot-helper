663c941fbb8630d19f54dbafbbde7432189ef12d
sak_shima <sak_shima@worksid.co.jp>
2022-10-10 12:50:53 +0900

    読み取り

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,29 @@
     <input type="text" v-model="name" placeholder="Todo name">
     <input type="text" v-model="description" placeholder="Todo description">
     <button v-on:click="createTodo">Create Todo</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import { API } from 'aws-amplify';
 import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 export default {
-  name: 'app',
+  name: 'App',
+  async created() {
+    this.getTodos();
+    this.subscribe();
+  },
   data() {
     return {
       name: '',
-      description: ''
+      description: '',
+      todos: []
     }
   },
   methods: {
@@ -24,13 +34,20 @@ export default {
       const { name, description } = this;
       if (!name || !description) return;
       const todo = { name, description };
+      this.todos = [...this.todos, todo];
       await API.graphql({
         query: createTodo,
         variables: {input: todo},
       });
       this.name = '';
       this.description = '';
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      });
+      this.todos = todos.data.listTodos.items;
     }
   }
-};
+}
 </script>
